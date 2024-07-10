0ebd2bfd18305bf8a26edb7ea9b8cc40a4faa4fa
KingaX1 <cass_03s@wp.pl>
2024-07-10 13:04:38 +0200

    2

--- task-1.js
+++ task-1.js
@@ -1,8 +1,12 @@
-function makeTransaction(quantity, pricePerDroid){
+function  makeTransaction(quantity, pricePerDroid){
     let totalPrice = quantity * pricePerDroid;
+    
+    return `You ordered ${quantity} droid worth ${totalPrice} credits!`;
 }
-return `You ordered ${quantity} droid worth ${pricePerDroid} credits!`;
 
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500)); 
 
 
 
